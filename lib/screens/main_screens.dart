043d988505bb3042b66c2e860d78169082cc4e65
David Jo <war2290@gmail.com>
2024-04-19 14:51:10 +0900

    feat: mainscreens

--- lib/screens/main_screens.dart
+++ lib/screens/main_screens.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainScreens extends StatefulWidget {
@@ -8,11 +9,51 @@ class MainScreens extends StatefulWidget {
 }
 
 class _MainScreensState extends State<MainScreens> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: const Text('MainScreens'),
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          Container( // index 0
+            color: Colors.orange[100],
+            child: Center(
+              child: Text(
+                'IndexedStack 1',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          ),
+          Container( // index 1
+            color: Colors.red[100],
+            child: Center(
+              child: Text(
+                'IndexedStack 2',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.chat_bubble),
+            label: '채팅',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        currentIndex: _selectedIndex,
       ),
     );
   }
