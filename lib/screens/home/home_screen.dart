510a3192522b5cb39ca2e9db2e9bfe7951cb3dcc
David Jo <war2290@gmail.com>
2024-04-19 15:26:45 +0900

    feat: appbar on homescreen step1

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fleamarket/theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('homeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeScreen appBar 영역(index:0)'),
+      ),
+      body: Container(
+        color: Colors.orange[100],
+        child: Center(
+          child: Text(
+            'HomeScreen body 영역(index:0)',
+            style: textTheme().displayMedium,
+          ),
+        ),
+      ),
     );
   }
 }
