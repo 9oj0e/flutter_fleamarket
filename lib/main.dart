e00e6f8b23b691e64ad4ce963ca55bed698a5f4c
David Jo <war2290@gmail.com>
2024-04-19 14:37:47 +0900

    docs: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fleamarket/screens/main_screens.dart';
+import 'package:flutter_fleamarket/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(FleaMarketUI());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FleaMarketUI extends StatelessWidget {
+  const FleaMarketUI({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'flea_market_ui',
+      debugShowCheckedModeBanner: true,
+      home: MainScreens(),
+      theme: theme(),
+    );
   }
 }
