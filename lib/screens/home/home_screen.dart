e61cd11bb8389c3e681ab42223e8cb2ee4fce9ac
David Jo <war2290@gmail.com>
2024-04-19 15:54:34 +0900

    docs: homescreen bound to components & an image load issue found

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_fleamarket/theme.dart';
+import 'package:flutter_fleamarket/models/product.dart';
+import 'package:flutter_fleamarket/screens/home/components/product_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -30,7 +31,20 @@ class HomeScreen extends StatelessWidget {
           child: Divider(thickness: 0.5, height: 0.5, color: Colors.grey),
         ),
       ),
-      body: Container(),
+      body: ListView.separated(
+        itemBuilder: (context, index) {
+          return ProductItem(
+            product: productList[index],
+          );
+        },
+        separatorBuilder: (context, index) => const Divider(
+          height: 0,
+          indent: 16,
+          endIndent: 16,
+          color: Colors.grey,
+        ),
+        itemCount: productList.length,
+      ),
     );
   }
 }
